b0000914ed11c5e74cb92af51b798748056106d8
Terry Carr <terry@terrycarr.dev>
2025-02-28 17:13:24 +0000

    add .gitignore, update module imports to use ES modules, and enhance database connection handling

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import apiRouter from './Routers/api'
-import './db'
+import apiRouter from './Routers/api.js'
+import './db.js'
 dotenv.config()
 
 const app = express()

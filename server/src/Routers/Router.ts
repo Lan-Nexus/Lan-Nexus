d97fdc1090726c6ceea21f0b956717f458e8b42c
Terry Carr <terry@brandoncross.co.uk>
2025-03-01 00:09:27 +0000

    refactor Router class to use specific request handler types for HTTP methods

--- server/src/Routers/Router.ts
+++ server/src/Routers/Router.ts
@@ -1,5 +1,11 @@
 import { Router as ExpressRouter } from 'express';
 import { Controller } from '../Controllers/Controller.js';
+
+type routerHandlerGet = 'list' | 'read';
+type routerHandlerPost = 'create';
+type routerHandlerPut = 'update';
+type routerHandlerDelete = 'delete';
+
 export default class Router<T extends Controller> {
   private router: ExpressRouter;
   private objects: any = {};
@@ -23,33 +29,33 @@ export default class Router<T extends Controller> {
     return this.objects[objName];
   }
 
-  public get<T extends Controller>(path: string, object: new () => T, callback: string) {
+  public get<T extends Controller>(path: string, object: new () => T, requestHandler: routerHandlerGet) {
     const obj = this.makeOrFindObject<T>(object);
-    this.router.get(path, (req, res) => obj[callback](req, res))
+    this.router.get(path, (req, res) => obj[requestHandler](req, res))
     return this;
   }
 
-  public post<T extends Controller>(path: string, object: new () => T, callback: string) {
+  public post<T extends Controller>(path: string, object: new () => T, requestHandler: routerHandlerPost) {
     const obj = this.makeOrFindObject<T>(object);
-    this.router.post(path, (req, res) => obj[callback](req, res))
+    this.router.post(path, (req, res) => obj[requestHandler](req, res))
     return this;
   }
 
-  public put<T extends Controller>(path: string, object: new () => T, callback: string) {
+  public put<T extends Controller>(path: string, object: new () => T, requestHandler: routerHandlerPut) {
     const obj = this.makeOrFindObject<T>(object);
-    this.router.put(path, (req, res) => obj[callback](req, res))
+    this.router.put(path, (req, res) => obj[requestHandler](req, res))
     return this;
   }
 
-  public delete<T extends Controller>(path: string, object: new () => T, callback: string) {
+  public delete<T extends Controller>(path: string, object: new () => T, requestHandler: routerHandlerDelete) {
     const obj = this.makeOrFindObject<T>(object);
-    this.router.delete(path, (req, res) => obj[callback](req, res))
+    this.router.delete(path, (req, res) => obj[requestHandler](req, res))
     return this;
   }
 
-  public all<T extends Controller>(path: string, object: new () => T, callback: string) {
+  public all<T extends Controller>(path: string, object: new () => T, requestHandler: string) {
     const obj = this.makeOrFindObject<T>(object);
-    this.router.all(path, (req, res) => obj[callback](req, res))
+    this.router.all(path, (req, res) => obj[requestHandler](req, res))
     return this;
   }
 

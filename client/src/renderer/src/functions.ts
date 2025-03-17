e78932894812fa238ea9e037fc1dbb8a85e7f854
Terry Carr <terry@brandoncross.co.uk>
2025-03-17 21:00:20 +0000

    typescript fixes.

--- client/src/renderer/src/functions.ts
+++ client/src/renderer/src/functions.ts
@@ -1,29 +1,48 @@
-export default new Proxy(
-  {},
-  {
-    get: function (_target, prop) {
-      return (progressCallback, ...args) => {
-        return new Promise((resolve, reject) => {
-          window.electron.ipcRenderer.send('function', {
-            functionName: prop,
-            args: args,
-          });
+type Progress = {
+  download: (
+    progressCallback: (percent: string, message: string) => void,
+    path: string,
+    filename: string
+  ) => Promise<void>;
+  unzip: (
+    progressCallback: (percent: string, message: string) => void,
+    path: string,
+    filename: string
+  ) => Promise<void>;
+  run: (
+    progressCallback: (percent: string, message: string) => void,
+    path: string,
+    command: string
+  ) => Promise<void>;
+  clearTemp: (progressCallback: (percent: string, message: string) => void) => Promise<void>;
+  removeGame: (
+    progressCallback: (percent: string, message: string) => void,
+    path: string
+  ) => Promise<void>;
+};
 
-          window.electron.ipcRenderer.once('function-reply', (_event, arg) => {
-            resolve(arg);
-          })
+export default new Proxy({} as Progress, {
+  get: function (_target, prop) {
+    return (progressCallback, ...args) => {
+      return new Promise((resolve, reject) => {
+        window.electron.ipcRenderer.send('function', {
+          functionName: prop,
+          args: args,
+        });
 
-          window.electron.ipcRenderer.once('function-error', (_event, arg) => {
-            reject(arg);
-          });
+        window.electron.ipcRenderer.once('function-reply', (_event, arg) => {
+          resolve(arg);
+        });
 
-          window.electron.ipcRenderer.on('function-progress', (_event, ...arg) => {
-            console.log('Progress:', arg);
-            progressCallback(...arg);
-          })
+        window.electron.ipcRenderer.once('function-error', (_event, arg) => {
+          reject(arg);
+        });
 
-        })
-      };
-    },
-  }
-);
+        window.electron.ipcRenderer.on('function-progress', (_event, ...arg) => {
+          console.log('Progress:', arg);
+          progressCallback(...arg);
+        });
+      });
+    };
+  },
+});
--- client/src/renderer/src/stores/useGameStore.ts
+++ client/src/renderer/src/stores/useGameStore.ts
@@ -1,111 +1,131 @@
-import { defineStore } from 'pinia'
-import axios from 'axios'
-import functions from '../functions.js'
-import { useProgressStore } from './useProgress.js'
+import { defineStore } from 'pinia';
+import axios from 'axios';
+import functions from '../functions.js';
+
+import { useProgressStore } from './useProgress.js';
+
+type archives = {
+  id: number;
+  name: string;
+  file: string;
+  isInstalled: boolean;
+};
 
 type gameState = {
-  description: string,
-  gameID: string,
-  id: number,
-  name: string,
-  selectedArchive: number,
-  type: string,
-  archives: unknown[]
-}
+  description: string;
+  gameID: string;
+  id: number;
+  name: string;
+  selectedArchive: number;
+  type: string;
+  archives: archives[];
+};
 
 export const useGameStore = defineStore('game', {
   state: () => ({
     games: [] as gameState[],
-    selectedGameId: 1
+    selectedGameId: 1,
   }),
   getters: {
     selectedGame: (state) => {
-      const game = state.games.find((game) => game.id === state.selectedGameId)
+      const game = state.games.find((game) => game.id === state.selectedGameId);
       if (game) {
-        console.log(game.name)
-        return game
+        console.log(game.name);
+        return game;
       }
-      return void 0
+      return void 0;
     },
     selectedArchive: (state) => {
-      const game = state.games.find((game) => game.id === state.selectedGameId)
+      const game = state.games.find((game) => game.id === state.selectedGameId);
       if (game) {
-        const archive = game.archives.find((archive) => archive.id === game.selectedArchive)
+        const archive = game.archives.find((archive) => archive.id === game.selectedArchive);
         if (archive) {
-          console.log(archive.name)
-          return archive
+          console.log(archive.name);
+          return archive;
         }
       }
-      return void 0
-    }
+      return void 0;
+    },
   },
   actions: {
     selectGame(id) {
-      this.selectedGameId = id
+      this.selectedGameId = id;
     },
-    async installArchive(archiveName) {
-      const game = this.games.find((game) => game.id === this.selectedGameId)
-      const archive = game!.archives.find((archive) => archive.id === game!.selectedArchive)
+    async installArchive() {
+      const game = this.games.find((game) => game.id === this.selectedGameId);
+      const archive = game!.archives.find((archive) => archive.id === game!.selectedArchive);
+      if (archive === void 0) {
+        return;
+      }
 
-      const safeName = game!.name.replaceAll(' ', '-')
-      const archiveFile = safeName + '/' + archive.name
+      const safeName = game!.name.replaceAll(' ', '-');
+      const archiveFile = safeName + '/' + archive.name;
 
-      const progressStore = useProgressStore()
-      progressStore.active = true
+      const progressStore = useProgressStore();
+      progressStore.active = true;
 
       try {
-        await functions.download(progressStore.setProgress, archive.file, archiveFile + '.zip')
-        await functions.unzip(progressStore.setProgress, archiveFile + '.zip', archiveFile)
-        await functions.run(progressStore.setProgress, archiveFile, 'install')
-        await functions.clearTemp(progressStore.setProgress)
+        await functions.download(progressStore.setProgress, archive.file, archiveFile + '.zip');
+        await functions.unzip(progressStore.setProgress, archiveFile + '.zip', archiveFile);
+        await functions.run(progressStore.setProgress, archiveFile, 'install');
+        await functions.clearTemp(progressStore.setProgress);
       } catch (error) {
-        console.error(error)
+        console.error(error);
       } finally {
-        progressStore.active = false
+        progressStore.active = false;
       }
 
-      archive.isInstalled = true
+      archive.isInstalled = true;
     },
-    async uninstallArchive(archiveName) {
-      const progressStore = useProgressStore()
-      progressStore.active = false
-      const game = this.games.find((game) => game.id === this.selectedGameId)
-      if (game === undefined) {
-        return
+    async uninstallArchive() {
+      const progressStore = useProgressStore();
+      progressStore.active = false;
+      const game = this.games.find((game) => game.id === this.selectedGameId);
+      if (game === void 0) {
+        return;
       }
-      const archive = game.archives.find((archive) => archive.id === game.selectedArchive)
+      const archive = game.archives.find((archive) => archive.id === game.selectedArchive);
+      if (archive === void 0) {
+        return;
+      }
 
-      const safeName = game.name.replaceAll(' ', '-')
-      const archiveFile = safeName + '/' + archive.name
+      const safeName = game.name.replaceAll(' ', '-');
+      const archiveFile = safeName + '/' + archive.name;
 
       try {
-        await functions.run(progressStore.setProgress, archiveFile, 'uninstall')
-        await functions.removeGame(progressStore.setProgress, archiveFile)
+        await functions.run(progressStore.setProgress, archiveFile, 'uninstall');
+        await functions.removeGame(progressStore.setProgress, archiveFile);
       } catch (error) {
-        console.error(error)
+        console.error(error);
       } finally {
-        progressStore.active = false
+        progressStore.active = false;
       }
 
-      archive.isInstalled = false
+      archive.isInstalled = false;
     },
     loadGames() {
       axios.get('http://localhost:3000/api/games').then((response) => {
-        this.games = response.data
-      })
+        this.games = response.data;
+      });
     },
     async play() {
-      const selectedGame = this.selectedGame
+      const selectedGame = this.selectedGame;
       if (selectedGame && selectedGame.type === 'steam') {
-        document.location.href = `steam://run/${selectedGame.gameID}`
+        document.location.href = `steam://run/${selectedGame.gameID}`;
       }
       if (selectedGame && selectedGame.type === 'zip') {
-        const game = this.games.find((game) => game.id === this.selectedGameId)
-        const archive = game!.archives.find((archive) => archive.id === game.selectedArchive)
-        const safeName = game!.name.replaceAll(' ', '-')
-        const archiveFile = safeName + '/' + archive.name
-        await functions.run(useProgressStore().setProgress, archiveFile, 'play')
+        const game = this.games.find((game) => game.id === this.selectedGameId);
+        if (game === void 0) {
+          return;
+        }
+        const archive = game!.archives.find((archive) => archive.id === game.selectedArchive);
+        if (archive === void 0) {
+          return;
+        }
+        const safeName = game!.name.replaceAll(' ', '-');
+        const archiveFile = safeName + '/' + archive.name;
+        await functions.run(useProgressStore().setProgress, archiveFile, 'play');
       }
-    }
-  }
-})
+    },
+  },
+});

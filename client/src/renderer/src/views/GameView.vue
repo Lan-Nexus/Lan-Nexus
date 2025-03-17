e4a3c64244c3b75a964558ab9da09c4cb70676bb
Terry Carr <terry@brandoncross.co.uk>
2025-03-17 22:04:56 +0000

    refactor: enhance GameView layout with parallax effect and dynamic margins

--- client/src/renderer/src/views/GameView.vue
+++ client/src/renderer/src/views/GameView.vue
@@ -15,41 +15,63 @@ const selectedGame = computed(() => {
 <template>
   <div class="flex flex-col h-full w-full flex flex-row">
     <SideNav />
-    <div class="flex flex-col flex-1" style="height: 87vh; overflow: overlay" v-if="selectedGame">
-      <div class="w-full h-60 relative">
-        <img
-          v-if="selectedGame.heroImage"
-          :src="'data:image/jpeg;base64,' + selectedGame.heroImage"
-          alt="game"
-          class="w-full object-cover"
-        />
-        <img
-          v-if="selectedGame.logo"
-          :src="'data:image/jpeg;base64,' + selectedGame.logo"
-          class="top-1/2 absolute w-1/3"
-        />
-      </div>
-      <div class="p-4 pt-52">
-        <div class="flex gap-4 items-center pb-4">
-          <img
-            v-if="selectedGame.icon"
-            :src="'data:image/jpeg;base64,' + selectedGame.icon"
-            alt="game icon"
-            class="h-12 w-12"
-          />
-          <h1 class="text-2xl">{{ selectedGame?.name }}</h1>
-          <span
-            class="badge"
-            :class="selectedGame.type === 'zip' ? 'badge-primary' : 'badge-secondary'"
-          >
-            {{ selectedGame.type }}
-          </span>
+    <div v-if="selectedGame" class="flex flex-col flex-1" style="height: 87vh; overflow: overlay">
+      <div class="parallax">
+        <div class="parallax__layer parallax__layer--back">
+          <div class="w-full relative">
+            <img
+              v-if="selectedGame.heroImage"
+              :src="'data:image/jpeg;base64,' + selectedGame.heroImage"
+              alt="game"
+              class="w-full"
+            />
+            <img
+              v-if="selectedGame.logo"
+              :src="'data:image/jpeg;base64,' + selectedGame.logo"
+              class="absolute bottom-5 left-5"
+            />
+          </div>
         </div>
-        <ActionBar />
-        <div class="mt-4 flex flex-row gap-4">
-          <div v-html="selectedGame?.description"></div>
+        <div class="parallax__layer parallax__layer--base">
+          <div class="dynmaic-margin bg-base-200">
+            <ActionBar />
+            <div class="mt-4 flex flex-row gap-4">
+              <div v-html="selectedGame?.description"></div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.dynmaic-margin {
+  margin: 32% 0;
+}
+.parallax {
+  perspective: 1px;
+  height: 100vh;
+  overflow-x: hidden;
+  overflow-y: auto;
+}
+
+.parallax__layer {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+}
+
+.parallax__layer--base {
+  transform: translateZ(0);
+}
+
+.parallax__layer--back {
+  transform: translateZ(-0.5px);
+  width: 150%;
+  left: -25%;
+  top: -25%;
+}
+</style>

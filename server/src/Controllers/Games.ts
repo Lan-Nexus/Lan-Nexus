2a2006efe3c8a41e3570fe1617359b3bb42ae3ec
Sam Cross <sam.cross@gmx.com>
2025-06-21 21:47:26 +0100

    add keys to api

--- server/src/Controllers/Games.ts
+++ server/src/Controllers/Games.ts
@@ -3,12 +3,21 @@ import {
   gamesSelectSchema,
   gamesUpdateSchema,
 } from "../db/schema.js";
-import { Request, Response } from "express";
 import GameModel from "../Models/Game.js";
+import GameKeyModel from "../Models/GameKey.js";
 import { ResourceController } from "./ResourceController.js";
+import { Request, Response } from "express";
 
 export default class GamesController extends ResourceController {
   constructor() {
     super(GameModel, gamesSelectSchema, gamesInsertSchema, gamesUpdateSchema);
+  }
+
+  async list(_req: Request, res: Response) {
+    const games = await this.model.list();
+    for (const game of games) {
+      game.keys = await GameKeyModel.listByGame(game.id);
+    }
+    res.send(games);
   }
 }

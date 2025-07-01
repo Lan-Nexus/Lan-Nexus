e4432ab89f2728e273c9319d9f04d1c8d2b6b8e6
Terry Carr <terry@brandoncross.co.uk>
2025-07-01 23:22:48 +0100

    fix: update edit button style from secondary to warning

--- server/src/renderer/components/games/gameItem.vue
+++ server/src/renderer/components/games/gameItem.vue
@@ -11,7 +11,7 @@ const { game } = defineProps<{ game: GameType }>();
     <td><img width="100px" :src="game.headerImage" /></td>
     <td class="text-end">
       <button class="btn m-1 btn-primary">View</button>
-      <button class="btn m-1 btn-secondary">Edit</button>
+      <button class="btn m-1 btn-warning">Edit</button>
       <button class="btn m-1 btn-error">Delete</button>
     </td>
   </tr>

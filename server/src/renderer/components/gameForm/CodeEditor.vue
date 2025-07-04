ee80a6d840dde9743f4445afdfccd396e84fefdd
Sam Cross <sam.cross@gmx.com>
2025-07-04 19:53:01 +0100

    use codemirror for code edditor

--- server/src/renderer/components/gameForm/CodeEditor.vue
+++ server/src/renderer/components/gameForm/CodeEditor.vue
@@ -1,8 +1,40 @@
 <script lang="ts" setup>
+import { ref, onMounted, watch } from "vue";
+import { EditorView, basicSetup } from "codemirror";
+import { javascript } from "@codemirror/lang-javascript";
+
 const prop = defineProps<{
   title?: string;
 }>();
 const model = defineModel<string>();
+const editor = ref();
+let view: EditorView | null = null;
+
+onMounted(() => {
+  if (view) return;
+  view = new EditorView({
+    parent: editor.value,
+    doc: model.value,
+    extensions: [
+      basicSetup,
+      javascript(),
+      EditorView.updateListener.of((v) => {
+        if (!v.docChanged) return
+        model.value = v.state.doc.toString();
+      })
+    ],
+  });
+});
+
+watch(
+  () => model.value,
+  (val) => {
+    if (!view || view.state.doc.toString() === val) return;
+    view.dispatch({
+      changes: { from: 0, to: view.state.doc.length, insert: val || "" }
+    });
+  }
+);
 </script>
 
 <template>
@@ -10,6 +42,6 @@ const model = defineModel<string>();
     <div class="mb-2">
       {{ prop.title || "Code Editor" }}
     </div>
-    <textarea v-model="model" class="h-60 w-full bg-base-200 base"></textarea>
+    <div ref="editor" class="h-60 w-full bg-base-200 base rounded"></div>
   </div>
 </template>
